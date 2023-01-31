4ef2c63e022cf2d9a1427a2907a678225169e630
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-01-30 19:52:04 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// $("h1").css("color", "red");
+// $("h1").css("font-size", "3rem");
+// $("h1").css("font-style", "italic");
+
+//using multiple
+// $("h1").css({ color: "green", "font-size": "3rem" });
+
+//using JS
+// document.querySelector("h1").classList.add("style1");
+
+//using JQuery
+$("h1").addClass("style1 style2");
